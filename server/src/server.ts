f2004b007e97540d2230194d197c73be1b349166
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-03-16 00:02:42 +0100

    Add: Db connection to mongodb atlas cluster.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,12 @@
 import app from "./app";
+import { config } from "dotenv";
+import { connectDb } from "./lib/db";
 
-app.listen(3030, () => {
-  console.log("Server is running on http://localhost:3030");
+config(); // Load environment variables from .env file
+connectDb(); // Connect to the database
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
