c236ffd5e147798e4c5fee3dd5db47d5c2dd23fa
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-04-16 22:11:03 +0100

    Add features to welcome page

---
+++ apps/frontend/src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const features = [
+  { icon: "group-chat", label: "Chatting" },
+  { icon: "send-image-solid", label: "Media" },
+  { icon: "privacy", label: "Privacy" },
+  { icon: "speed", label: "Speed" },
+];
